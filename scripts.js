94966ac3f096d8eace1baf7159ed9059ff13459c
John Botero <john.botero@polygon.us>
2021-09-24 12:31:07 -0500

    updates styles #2

--- scripts.js
+++ scripts.js
@@ -3,4 +3,4 @@ let scene = document.getElementById("scene")
 let parallaxInstance = new Parallax(scene, {
     relativeInput: true,
 })
-parallaxInstance.friction(0.1, 0.1)
+parallaxInstance.friction(0.05, 0.05)
